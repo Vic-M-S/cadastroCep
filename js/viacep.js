7550e7465300f92e446097e9042d689aee4ec5d0
Vic-M-S <victoriasantiago450@gmail.com>
2025-08-19 19:01:05 -0300

    Initial commit

---
+++ js/viacep.js
@@ -0,0 +1,30 @@
+function pesquisa() {
+    const cep = document.getElementById("cep").value;
+    if(cep.length!=9){
+        return;
+    }
+    const xhttp = new XMLHttpRequest();
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    xhttp.open("GET", url);
+    xhttp.responseType = 'json';
+    xhttp.send();
+    console.log(xhttp.response);
+    xhttp.onload = function (){
+        const dados = xhttp.response;
+        console.log(dados);
+        if(dados.erro){
+            alert("CEP não encontrado");
+            return
+        }
+        document.getElementById("endereco").value = dados.logradouro;
+        document.getElementById("bairro").value = dados.bairro;
+        document.getElementById("cidade").value = dados.localidade;
+        document.getElementById("uf").value = dados.uf;
+        
+
+
+
+    }
+}
+
+
